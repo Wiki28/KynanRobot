feb593c3c13df157b62c1958f23d3d1de76f2085
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-03-10 18:29:41 +0800

    Update ping.py

--- KynanRobot/modules/ping.py
+++ KynanRobot/modules/ping.py
@@ -46,9 +46,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ❤️\n"
-        "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
-        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "I Am Alive 🥲\n"
+        "<b>Time Taken:</b> <code>{}</code>\n"
+        "<b>Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
