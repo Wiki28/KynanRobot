ba0b007d0dfc6697ed8470d75a4dbad835150c63
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-03-24 04:22:02 +0800

    Update ping.py

--- KynanRobot/modules/ping.py
+++ KynanRobot/modules/ping.py
@@ -40,13 +40,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ....​")
+    message = msg.reply_text("🏓Pinging....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "I Am Alive 🥲\n"
+        "I Am Alive Darling ❤️\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
